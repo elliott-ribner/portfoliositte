ca2dc3a290ce2b61575ee9a8801be1e78c303870
elliott-ribner <elr886@gmail.com>
2015-04-20 20:17:21 -0400

    first commit

---
+++ js/custom-portfolio-slider.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  //initialize references to div
+  var a = 0;
+  var b = a+3;
+
+  //check to see if numbers exceed total number of divs(slides)
+  function plusOneOrReset(number){
+    number += 1;
+    if (number > 6){
+      number = 1;
+    }
+    return number;
+  };
+
+  //toggle slides to hidden and call
+  function hideSlides(){
+    a = plusOneOrReset(a);
+    b = plusOneOrReset(b);
+    $('#port'+ String(a) ).toggleClass('hidden');
+    $('#port'+ String(b)).toggleClass('hidden');
+  };
+
+  window.setInterval(function(){
+    hideSlides();
+  }, 5000);
+  // hideSlides();
+
+
+  //toggle portfolio
+    $(function() {
+
+      $('.img').each(function(n) {
+
+        $(this).wrap('<figure class="tint t1"></figure>');
+      });
+
+    });
+
+});
+
+function toggleport(){
+    $('#port-extended').toggleClass('hidden');
+  };
+
